789f15b93bf1486a54d2d66c28036b9029e4c728
aramay <kasper_341@hotmail.com>
2021-07-15 11:03:33 -0700

    add tests for /location-search route

--- __tests__/test-Middleware.js
+++ __tests__/test-Middleware.js
@@ -10,52 +10,67 @@ const server = 'http://localhost:3000/api';
 
 describe('Route integration', () => {
   let mockAPI;
+
+  const searchResults = [
+    {
+      description: 'Mountain View, CA 94043, USA',
+      matched_substrings: [ [null] ],
+      place_id: 'ChIJoQmen1G3j4ARbhoe7nVBEoQ',
+      reference: 'ChIJoQmen1G3j4ARbhoe7nVBEoQ',
+      structured_formatting: {
+        main_text: '94043',
+        main_text_matched_substrings: [null],
+        secondary_text: 'Mountain View, CA, USA'
+      },
+      terms: [ [null], [null], [null], [null] ],
+      types: [ 'postal_code', 'geocode' ]
+    }
+  ]
+
   beforeAll( () => {
-    // mock endpoint /location-search
+    // mock endpoint http://localhost:3000/api
     mockAPI = nock(server)
   })
   
-  describe('/', () => {
-    it('should return App object', () => {
-      return request(server)
-        .get('/')
-        .expect('Content-Type', /text\/html/)
-        .expect(200)
-    })
-  })
-
   describe('POST /location-search', () => {
-     const searchResults = [
-       {
-         description: 'Mountain View, CA 94043, USA',
-         matched_substrings: [ [Object] ],
-         place_id: 'ChIJoQmen1G3j4ARbhoe7nVBEoQ',
-         reference: 'ChIJoQmen1G3j4ARbhoe7nVBEoQ',
-         structured_formatting: {
-           main_text: '94043',
-           main_text_matched_substrings: [Array],
-           secondary_text: 'Mountain View, CA, USA'
-         },
-         terms: [ [Object], [Object], [Object], [Object] ],
-         types: [ 'postal_code', 'geocode' ]
-       }
-     ]
-
-    it('responds with location address', (done) => {
-
+    it('should return an array of searchResults', (done) => {
       // define the method to be intercepted
       mockAPI.post('/location-search')
       // respond with ok and specified json response
       .reply(200, {
-        "message": 'mountain view'
+        "searchResults": searchResults
+      });
+
+      request(server)
+        .post('/location-search')
+        .send('95814')
+        .then(res => {
+          const result = JSON.parse(res.text)
+          assert(res.statusCode, 200)
+          expect(result.searchResults).to.be.an('array')
+          done()
+        })
+        .catch(err => done(err))
+    })
+  })
+
+  describe('POST /location-search', () => {
+    it('responds with location potential address in zip code', (done) => {
+      // define the method to be intercepted
+      mockAPI.post('/location-search')
+      // respond with ok and specified json response
+      .reply(200, {
+        "searchResults": searchResults
       });
       
       request(server)
         .post('/location-search')
         .send('95814')
         .then(res => {
-          // console.log(res)
+          const result = JSON.parse(res.text)
+          // console.log(result)
           assert(res.statusCode, 200)
+          expect(searchResults[0]).to.have.all.keys("description", "matched_substrings", "place_id", "reference", "structured_formatting", "terms", "types")
           done()
 
         })
