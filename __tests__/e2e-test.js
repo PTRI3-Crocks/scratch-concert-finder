ff7edda1b82c7262ac3e882439c4efd8a37d4b84
aramay <kasper_341@hotmail.com>
2021-07-16 17:01:39 -0700

    add e2e test

--- __tests__/e2e-test.js
+++ __tests__/e2e-test.js
@@ -8,7 +8,8 @@ describe('Front-end Integration/Features', () => {
 
   beforeAll( async () => {
     browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: false
     })
     jest.setTimeout(10000) 
     page = await browser.newPage()
@@ -22,6 +23,18 @@ describe('Front-end Integration/Features', () => {
     it('page loads successfully', async () => {
       await page.goto(APP)
       await expect(page.title()).resolves.toMatch('In The Loop')
+    })
+  })
+  describe('User can search concerts via zip code', () => {
+    it('it should display a list of concerts', async () => {
+      await page.goto(APP)
+      page.waitForSelector('div.title svg', {visible: true})
+      const svg = await page.$$('div.title svg')
+      if(svg) {
+        await svg.click()
+      }
+      console.log('span ', spans)
+      await expect("hello").resolves.toMatch(spans)
     })
   })
 })
--- cypress/integration/1-getting-started/e2e_spec.js
+++ cypress/integration/1-getting-started/e2e_spec.js
@@ -11,17 +11,29 @@ describe('example to-do app', () => {
       .should('have.text', 'In The Loop')
   })
 
-
-
-  it('User can enter zip code in search bar', () => {
+  it('User can lookup places in given zipcode', () => {
     // get input tag
     // type zip code
-    // press enter to get list of plces
+    // press enter to get list of places
+    cy.get('input').should('have.class', 'chakra-input')
+      .wait(3000)
+      .click()
+      .type('94043')
+      .type('{enter}')
+    
+    cy.get('div.placesPanel').find('div').its('length').should('be.gte', 1)
+  })
+
+  it('User can lookup concerts in a give place', () => {
     cy.get('input').should('have.class', 'chakra-input')
       .wait(3000)
       .click()
       .type('94043')
       .type('{enter}')
 
+    cy.get('div.placesPanel').find('div').first().click()
+    
+    cy.get('div.MuiGrid-grid-xs-4 div.MuiContainer-root').find('div').its('length').should('be.gte', 1)
+      
   })
 })
